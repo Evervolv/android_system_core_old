55b87011033f5f17b5313806e156f826866b14da
Prashant Somashekar <prashsomash@gmail.com>
2011-12-17 17:26:54 -0500

    init: add detection of charging mode
    -when BOARD_CHARGING_MODE_BOOTING_LPM is set to a path_to_sysfs_attribute
     then it and lpm.rc will be read accordingly.
    
    -adapted from techomancer's original change:
     (add detection of charging mode, remove the code for parsing param.lfs,
      since it is not needed)
     http://goo.gl/I19GG
    
    Change-Id: I6dab7438830b093bfa623b4eb8adb0c592cb45c3

--- init/init.c
+++ init/init.c
@@ -840,6 +840,25 @@ int audit_callback(void *data, security_class_t cls, char *buf, size_t len)
 
 #endif
 
+static int charging_mode_booting(void)
+{
+#ifndef BOARD_CHARGING_MODE_BOOTING_LPM
+	return 0;
+#else
+	int f;
+	char cmb;
+	f = open(BOARD_CHARGING_MODE_BOOTING_LPM, O_RDONLY);
+	if (f < 0)
+		return 0;
+
+	if (1 != read(f, (void *)&cmb,1))
+		return 0;
+
+	close(f);
+	return ('1' == cmb);
+#endif
+}
+
 int main(int argc, char **argv)
 {
     int fd_count = 0;
@@ -927,7 +946,11 @@ int main(int argc, char **argv)
         property_load_boot_defaults();
 
     INFO("reading config file\n");
-    init_parse_config_file("/init.rc");
+
+    if (!charging_mode_booting())
+       init_parse_config_file("/init.rc");
+    else
+       init_parse_config_file("/lpm.rc");
 
     /* Check for a target specific initialisation file and read if present */
     if (access("/init.target.rc", R_OK) == 0) {
