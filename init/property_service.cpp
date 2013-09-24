be429365f3b655f5d7d1b5ba4e76c4abd8d586bb
David Ng <dave@codeaurora.org>
2013-09-23 18:50:24 -0700

    init: Add vendor-specific initialization hooks.
    Allow optional vendor-specific initializations
    within init.  This can be used for runtime
    initialization setup that init rc scripts do
    not support.
    
    Change-Id: I7623a0d59b18f9ec8e3623958e2f7ccd72b877bf

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -55,6 +55,7 @@
 #include "init.h"
 #include "util.h"
 #include "log.h"
+#include "vendor_init.h"
 
 using android::base::StringPrintf;
 
@@ -660,6 +661,10 @@ void load_system_props() {
     load_properties_from_file("/odm/build.prop", NULL);
     load_properties_from_file("/vendor/build.prop", NULL);
     load_properties_from_file("/factory/factory.prop", "ro.*");
+
+    // Update with vendor-specific property runtime overrides
+    vendor_load_properties();
+
     load_recovery_id_prop();
 }
 
