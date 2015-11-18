ca7eca7d160fb2edff093fbf30bad24410ca534a
Nick Reuter <nreuter85@gmail.com>
2015-11-18 06:15:55 -0500

    mkimage: link to libz
    Change-Id: I9dee6569f1e5598f1e3ae59df5536c0b1da520eb

--- mkimage/Android.mk
+++ mkimage/Android.mk
@@ -1,7 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := mkimage.c crc32.c
+LOCAL_STATIC_LIBRARIES := libz
+LOCAL_SRC_FILES := mkimage.c
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := mkimage
 
